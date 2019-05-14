9919e7fd60238a98deb172810e53463d7c834eb6
unknown <k.ostrynska@gmail.com>
2019-05-14 12:53:33 +0200

    Expensify app core

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+// entry -> output
+//entry - where webpack should start (for us it's app.js in src folder)
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env'
+                    ]
+                }
+            }
+        }, {
+            test: /\.s?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 8080
+    },
+    mode: 'development'
+};
