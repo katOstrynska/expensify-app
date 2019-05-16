3463574e6d2a488e5b8ae005cce1e804b27268a4
unknown <k.ostrynska@gmail.com>
2019-05-16 12:20:19 +0200

    Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,39 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }, action) => {
+//Action denerators
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+});
+
+const setCount = ({ count } = {}) => ({
+    type: 'SET',
+    count
+});
+
+const resetCount = () => ({
+    type: 'RESET'
+});
+
+//Reducers
+// 1. Reducers are oure functions
+// 2. Never change state or action
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-            count: state.count + incrementBy
+            count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'SET':
             return {
@@ -23,32 +46,26 @@ const store = createStore((state = { count: 0 }, action) => {
         default:
             return state;
     }
-});
-
+};
+const store = createStore(countReducer);
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
 });
 
 
 
-store.dispatch({
-    type: 'INCREMENT', //convention for naming redux actions
-    incrementBy: 5
-});
-store.dispatch({
-    type: 'INCREMENT' //convention for naming redux actions
-});
-store.dispatch({
-    type: 'RESET'
-});
-store.dispatch({
-    type: 'DECREMENT'
-});
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+// store.dispatch({
+//     type: 'INCREMENT', //convention for naming redux actions
+//     incrementBy: 5
+// });
+store.dispatch(incrementCount({ incrementBy: 5 }));
+
+store.dispatch(incrementCount());
+
+// store.dispatch({
+//     type: 'INCREMENT' //convention for naming redux actions
+// });
+store.dispatch(resetCount());
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
+store.dispatch(setCount({ count: 101 }));
