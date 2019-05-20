002b810afe82f8f0f777875f089efb4ee1391cb4
unknown <k.ostrynska@gmail.com>
2019-05-20 17:01:48 +0200

    Expensify snapshots, tests

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFoudPage = () => (
+const NotFoundPage = () => (
     <div>
         404! - <Link to="/">Go home</Link>
     </div>
 );
 
-export default NotFoudPage;
+export default NotFoundPage;
